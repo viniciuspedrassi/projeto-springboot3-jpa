97eaada145dd75f1f35185de9ee78b142a0e7483
viniciuspedrassi <mviniciuscpedrassi@gmail.com>
2025-06-04 23:53:52 -0300

    Projeto Criado

---
+++ src/test/java/com/viniciuspedrassi/projeto/web/services/ProjetoWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciuspedrassi.projeto.web.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
