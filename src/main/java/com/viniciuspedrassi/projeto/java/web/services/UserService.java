136cdede704aa746478e129f063f095c961ca8af
viniciuspedrassi <mviniciuscpedrassi@gmail.com>
2025-06-14 19:10:14 -0300

    Tratamento de exceção - update

--- src/main/java/com/viniciuspedrassi/projeto/java/web/services/UserService.java
+++ src/main/java/com/viniciuspedrassi/projeto/java/web/services/UserService.java
@@ -13,6 +13,8 @@ import com.viniciuspedrassi.projeto.java.web.repositories.UserRepository;
 import com.viniciuspedrassi.projeto.java.web.services.exceptions.DatabaseException;
 import com.viniciuspedrassi.projeto.java.web.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -46,9 +48,13 @@ public class UserService {
 
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
